e5eed0a240d0a14cc16024f3c915043a01eea9a5
MDarbySmith <dyzun@uga.edu>
2016-07-14 14:25:45 -0400

    set up email intent NOT TESTED
    Signed-off-by: MDarbySmith <dyzun@uga.edu>

--- Application/src/main/java/com/example/android/slidingtabsbasic/InviteActivity.java
+++ Application/src/main/java/com/example/android/slidingtabsbasic/InviteActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.slidingtabsbasic;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,11 +19,18 @@ public class InviteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Invitation to We Are Henry");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello there, I'd like to invite you to try the We Are Henry app!");
+                String recipient = "mailto:"+R.id.editText2;
+                intent.setData(Uri.parse(recipient)); // or just "mailto:" for blank
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+                startActivity(intent);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
